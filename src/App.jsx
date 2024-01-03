b6d83ffc97eeaabe116a13f513f172141594d4a6
Benquerer <andrebenquerer@gmail.com>
2024-01-03 23:11:41 +0000

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-
+import { Routes, Route } from "react-router-dom"
+import { Navbar } from "./components/Navbar"
+import "./styles/App.css"
+import { About,Contacts,Compare,Home  } from "./pages"
 
 function App() {
-  
+
   return (
-    <h1>Hello Fit World!</h1>
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/compare" element={<Compare />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import "../styles/Navbar.css"
+
+export const Navbar = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <nav>
+            <Link to="/" className="title">FitArena</Link>
+            <div className="menu" onClick={() => {
+                setMenuOpen(!menuOpen)
+            }}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul className={menuOpen ? "open":""}>
+                <li>
+                    <NavLink to="/compare">Compare</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about">About</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/contacts">Contacts</NavLink>
+                </li>
+            </ul>
+        </nav>
+    )
+}
