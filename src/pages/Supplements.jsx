1500ebb8c92a5c04769fc189335725c00d9fb513
Benquerer <andrebenquerer@gmail.com>
2024-01-04 19:46:24 +0000

    Gallery

--- src/pages/Supplements.jsx
+++ src/pages/Supplements.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Gallery } from '../components/Gallery'
 
 export const Supplements = () => {
+
   return (
-    <div>Supplements</div>
+    <div>
+      <Gallery />
+    </div>
   )
 }
